449f596b0bb61b5f48e9234db7e62410f2303971
tradennn1 <treidnnn@gmail.com>
2022-08-07 16:51:40 +0600

    add new  class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,6 @@ public class Main {
 
         Dog dog1 = new Dog("Патрик", "Сибаину", ColorEnum.BROWN,romashka );
         System.out.println(dog1.getInfo());
-
         Ocean ocean = new Ocean(55);
         Kit kit = new Kit("Megaladon", "Kit", age.THIRTEEN,ocean);
         System.out.println(kit.getInfo());
